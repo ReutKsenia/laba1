015b66d41600c0088b983661d84926413805e07e
Reut Ksenia <rieut.ksenia@mail.ru>
2018-09-11 18:27:10 +0300

    second commit

--- lab1.1/Program.cs
+++ lab1.1/Program.cs
@@ -10,6 +10,11 @@ namespace lab1._1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Эта программа поможет подобрать вам фильм для вечернего просмотра.");
+            Console.WriteLine("Как вас зовут?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Здравствуйте, " + name + "!");
         }
     }
 }
